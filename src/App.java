b1c76ec4485db42226ff6e2c2acd8aad3d3bdad8
Aaron <26afreedmannaditch@gannacademy.org>
2025-05-02 16:47:41 -0400

    51 words

--- src/App.java
+++ src/App.java
@@ -5,10 +5,38 @@ and a text file that contains the entire story. Your job is to read the file lin
 line and use appropriate skills to see count the number of words. How you do this is up to you!
 */
 
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        ArrayList<String> words = new ArrayList<>();
+        try (Scanner scanner = new Scanner(Paths.get("greenEggsAndHam.txt"))) {
 
+            while (scanner.hasNextLine()) {
+                String row = scanner.nextLine();
+                row = row.toLowerCase();
+                row = row.replaceAll("-", " ");
+                row = row.replaceAll("\\p{Punct}", "");
+                String separateWords[] = row.split(" ");
+                if (!row.isEmpty()){
+                for (int index = 0; index < separateWords.length; index++) {
+                    if (words.contains(separateWords[index])) {
+                    } else {
+                        words.add(separateWords[index]);
+                    }
+                }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        for (int index = 0; index < words.size(); index++) {
+            System.out.println(words.get(index));
+        }
+        System.out.println(words.size() + " words");
+    }
 }
